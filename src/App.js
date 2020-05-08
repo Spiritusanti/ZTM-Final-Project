076a4efe91f5cfea2e54ee372bd0866dacc43ecf
Spiritusanti <mccrackenjacob@gmail.com>
2020-05-08 16:50:15 -0700

    adding functionality and facerecognition component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Particles from 'react-particles-js';
+import Clarifai from 'clarifai';
 import Navigation from'./components/Navigation/Navigation';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition'
 import Logo from'./components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
-import Particles from 'react-particles-js';
+
 import './App.css';
 
+
+const app = new Clarifai.App({
+  apiKey: "be0d8e9baead437881a11873fbc75b73"
+})
 
 const particlesOptions = {
  particles: {
@@ -19,19 +26,47 @@ const particlesOptions = {
   }
 }
 
-function App() {
-  return (
-    <div className="App">
-      <Particles className='particles'
-        params={particlesOptions} />
-      <Navigation />
-      <Logo />
-      <Rank/>
-      <ImageLinkForm />
-      {// <FaceRecognition />
-     }
-    </div>
+ class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+      imageUrl: ''
+    }
+  }
+
+onInputChange = (e) => {
+  this.setState({input: e.target.value});
+}
+
+onButtonSubmit = () => {
+  this.setState({imageUrl: this.state.input})
+  app.models.predict(Clarifai.FACE_DETECT_MODEL, this.state.input).then(
+    function(response) {
+      console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
+    },
+    function(err) {
+      // there was an error
+    }
   );
+}
+  
+  render() {
+    return (
+      <div className="App">
+        <Particles className='particles'
+          params={particlesOptions} />
+        <Navigation />
+        <Logo />
+        <Rank/>
+        <ImageLinkForm onInputChange={this.onInputChange} 
+        onButtonSubmit={this.onButtonSubmit}
+        />
+        <FaceRecognition imageUrl={this.state.imageUrl}/>
+      
+      </div>
+    );
+  }
 }
 
 export default App;
